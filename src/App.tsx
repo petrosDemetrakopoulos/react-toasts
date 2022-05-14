c10c10a5907bd36de1250c1948d58de4e45f2d51
Petros <psdeme@hotmail.com>
2022-05-14 20:23:39 +0300

    added properties

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,17 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  const notifyDefault = () => toast("Wow so easy!");
+  const notifyDefault = () => toast("A toast alert!");
+  const notifyTimeout = () => toast("Dismiss after 8 secs", {autoClose: 8000});
+  const notifyTopLeft = () => toast("This appears on top left corner", {position: 'top-left'});
+  const notifyNotBar = () => toast("This does not have the bar", {hideProgressBar: true});
   return (
     <div className="App">
       <div style={{ marginTop: '2em' }}>
-        <button onClick={notifyDefault}>Show default toast</button>
+        <button onClick={notifyDefault}>Show default toast</button> <br></br>
+        <button onClick={notifyTimeout}>Dismiss after 8 seconds</button>  <br></br>
+        <button onClick={notifyTopLeft}>Appears top left</button>  <br></br>
+        <button onClick={notifyNotBar}>No bar</button>
       </div>
       <ToastContainer />
     </div>
